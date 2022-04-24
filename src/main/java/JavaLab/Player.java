dd0e3ab2895365b063d2fa75c4d6ca0c89e6b80c
glopezfrutos <gus.lopezfrutos@gmail.com>
2022-04-24 20:03:03 -0300

    REFACTOR --- Classes main, Player and Game

---
+++ src/main/java/JavaLab/Player.java
@@ -0,0 +1,15 @@
+package JavaLab;
+
+import java.util.Scanner;
+
+public class Players {
+    private String name;
+
+    public Players(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
